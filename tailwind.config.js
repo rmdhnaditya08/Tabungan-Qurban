92fd96d0161696b7378333598434f7ff2071296a
Najib Bahrudin <bahrudinmohammadnajib@gmail.com>
2026-07-01 15:49:38 +0700

    chore: migrate configuration from Vite to Next.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
